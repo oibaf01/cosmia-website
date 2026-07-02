667da5343f69fbe02b4f5c6286c684d0748c4149
oibaf01 <fabiopastore27@gmail.com>
2026-07-02 22:25:37 +0200

    revert: ripristina rilevamento automatico lingua browser (utile per stranieri)

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -3,7 +3,6 @@ import { defineRouting } from 'next-intl/routing';
 export const routing = defineRouting({
   locales: ['it', 'en', 'fr', 'de'],
   defaultLocale: 'it',
-  localeDetection: false, // ignora Accept-Language del browser, usa solo defaultLocale
 });
 
 export type Locale = (typeof routing.locales)[number];
